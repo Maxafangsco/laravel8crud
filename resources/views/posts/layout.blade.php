6c92ef3b6e3ce61dd77a1d6fde7621a557b154cf
Maxafangsco <39309700+Maxafangsco@users.noreply.github.com>
2021-03-27 07:05:29 +0400

    about to chnage branch

--- resources/views/posts/layout.blade.php
+++ resources/views/posts/layout.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel 8 CRUD Application - laravelcode.com</title>
+    <title>Laravel 8 CRUD Application - laravel</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
   
 <div class="container">
-    @yield('content')
+@yield('content')
 </div>
    
 </body>
