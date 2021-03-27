c77b796cc1e15d4dbdfa1a09816930207667f9fd
Maxafangsco <39309700+Maxafangsco@users.noreply.github.com>
2021-03-27 07:37:40 +0400

    installing jetstream package

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 
 
 Route::resource('posts', PostController::class);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
